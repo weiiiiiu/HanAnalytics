b713eff1f514839ed1c5c46af2acf67d2f1452bc
wei zhong <weeiii826@gmail.com>
2024-12-09 14:06:24 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,18 @@
       <div class="main">
         <div class="logo">
           <img src="./assets/favicon.ico">
-          <span>Web Analytics</span>
+          <span>Han Analytics</span>
         </div>
-        <h2>Web分析</h2>
+        <h2>简单优雅的Web分析</h2>
       </div>
     </header>
     <main>
       <header>
         <Alert>
           <AlertDescription>
-            <p style="font-weight: bold;">· 开源地址: <a class="git-link" href="https://github.com/weiiiiiu/HanAnalytics"
+            <p>· Han Analytics 是一个简单的网络分析跟踪器和仪表板，托管在被称为赛博菩萨的 Cloudflare 上,无成本稳定运行,每天可达10万次免费统计。</p>
+            <p>· 域名、服务器、数据库 通通都不用! 托管在 Cloudflare Pages 上即可快速部署网站分析仪表板。</p>
+            <p style="font-weight: bold;">· 开源地址: <a class="git-link" href="https://github.com/uxiaohan/HanAnalytics"
                 target="_blank">Han-Analytics</a>
             </p>
           </AlertDescription>
@@ -83,9 +85,153 @@
 
         <div ref="echartsDOM" class="data-view"></div>
 
-        <!-- ... 其他模板代码保持不变 ... -->
+        <div class="pt-20 grid md:grid-cols-2 sm:grid-cols-1 gap-[16px]">
+          <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
+            <CardHeader>
+              <CardTitle>Pages</CardTitle>
+            </CardHeader>
+            <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
+              <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.path != undefined">
+                <p class="page-item" v-for="(i, idx) in resData.path" :key="idx">
+                  <span class="line-clamp-1">{{ i.name }}</span>
+                  <span class="line-clamp-1">{{ i.value }}</span>
+                  <em>{{ i.per }}<i :style="{ width: i.per }"></i></em>
+                </p>
+              </ScrollArea>
+              <div class="space-y-4 pt-8 w-full" v-else>
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-full" />
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
+            <CardHeader>
+              <CardTitle>Referrers</CardTitle>
+            </CardHeader>
+            <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
+              <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.referrer != undefined">
+                <p class="page-item" v-for="(i, idx) in resData.referrer" :key="idx">
+                  <img v-if="i.name" :src="getIconUrl(i.name)">
+                  <a :href="i.name" target="_blank" rel="noopener noreferrer" class="line-clamp-1 cursor-pointer">
+                    {{ i.name || '(None)' }}
+                  </a>
+                  <span class="line-clamp-1">{{ i.value }}</span>
+                  <em>{{ i.per }}<i :style="{ width: i.per }"></i></em>
+                </p>
+              </ScrollArea>
+              <div class="space-y-4 pt-8 w-full" v-else>
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-full" />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+
+        <div class="pt-6 grid xl:grid-cols-3 gap-[16px] md:grid-cols-2 sm:grid-cols-1">
+          <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
+            <CardHeader>
+              <CardTitle>Browsers</CardTitle>
+            </CardHeader>
+            <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
+              <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.soft != undefined">
+                <p class="page-item" v-for="(i, idx) in resData.soft" :key="idx">
+                  <span class="line-clamp-1">{{ i.name }}</span>
+                  <span class="line-clamp-1">{{ i.value }}</span>
+                  <em>{{ i.per }}<i :style="{ width: i.per }"></i></em>
+                </p>
+              </ScrollArea>
+              <div class="space-y-4 pt-8 w-full" v-else>
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-full" />
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
+            <CardHeader>
+              <CardTitle>OS</CardTitle>
+            </CardHeader>
+            <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
+              <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.os != undefined">
+                <p class="page-item" v-for="(i, idx) in resData.os" :key="idx">
+                  <img class="os" :src="getIcon(i.name)">
+                  <span class="line-clamp-1">{{ i.name }}</span>
+                  <span class="line-clamp-1">{{ i.value }}</span>
+                  <em>{{ i.per }}<i :style="{ width: i.per }"></i></em>
+                </p>
+              </ScrollArea>
+              <div class="space-y-4 pt-8 w-full" v-else>
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-full" />
+              </div>
+            </CardContent>
+          </Card>
+
+
+          <Card class="box-border flex flex-col w-full h-[460px] overflow-hidden">
+            <CardHeader>
+              <CardTitle>Areas</CardTitle>
+            </CardHeader>
+            <CardContent class="box-border pt-0 w-full h-full overflow-hidden">
+              <ScrollArea class="box-border p-2 pt-0 h-full w-full pages-list" v-if="resData.area != undefined">
+                <p class="page-item" v-for="(i, idx) in resData.area" :key="idx">
+                  <img :src="getIcon(i.name)">
+                  <span class="line-clamp-1">{{ i.code }}</span>
+                  <span class="line-clamp-1">{{ i.value }}</span>
+                  <em>{{ i.per }}<i :style="{ width: i.per }"></i></em>
+                </p>
+              </ScrollArea>
+              <div class="space-y-4 pt-8 w-full" v-else>
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-60" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-100" />
+                <Skeleton class="h-6 w-80" />
+                <Skeleton class="h-6 w-full" />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </section>
     </main>
+    <footer>
+      <p><img src="./assets/svg/ing.svg"></p>
+      <p>
+        <a href="https://pages.cloudflare.com" target="_blank" rel="noopener noreferrer"><img
+            src="./assets/svg/framework.svg"></a>
+        <a href="https://www.cloudflare.com/zh-cn/application-services/products/cdn/" target="_blank"
+          rel="noopener noreferrer"><img src="./assets/svg/cdn.svg"></a>
+        <a href="https://vuejs.org" target="_blank" rel="noopener noreferrer"><img src="./assets/svg/web.svg"></a>
+        <a href="https://api.vvhan.com" target="_blank"><img src="./assets/svg/surppot.svg"></a>
+      </p>
+    </footer>
   </section>
   <div class="z-[999999999]">
     <Toaster />
@@ -107,10 +253,10 @@
   </AlertDialog>
 </template>
 
+
 <script setup lang="ts">
 import { ref, markRaw, onMounted } from 'vue'
-import * as echarts from "echarts"
-import vh from 'vh-plugin'
+import * as echarts from "echarts";
 import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-vue-next'
 import { Skeleton } from '@/components/ui/skeleton'
@@ -118,134 +264,115 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue, } from '@/components/ui/select'
 import { Alert, AlertDescription } from '@/components/ui/alert'
+import vh from 'vh-plugin'
 import { Toaster } from '@/components/ui/toast'
 import { useToast } from '@/components/ui/toast/use-toast'
+const { toast } = useToast();
+// 弹窗
 import { AlertDialog, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, } from '@/components/ui/alert-dialog'
 import { Input } from '@/components/ui/input'
 
-const { toast } = useToast()
-
-// 登录相关
+// 登录
 const authStatus = ref<boolean>(false)
 const session = ref<string>(localStorage.getItem('session') || '')
 const loginStatus = ref<boolean>(false)
 const loginPassword = ref<string>('')
 const loginFn = async () => {
-  if (!loginPassword.value) return toast({ description: '请输入密码', variant: 'destructive' })
-  loginStatus.value = true
-  const res = await fetch('/api', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ type: 'Login', session: loginPassword.value })
-  })
+  if (!loginPassword.value) return toast({ description: '请输入密码', variant: 'destructive' });
+  loginStatus.value = true;
+  const res = await fetch('/api', { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify({ type: 'Login', session: loginPassword.value }) })
   await new Promise(resolve => setTimeout(resolve, 666))
-  loginStatus.value = false
+  loginStatus.value = false;
   const data = await res.json()
-  if (!data.success) return toast({ description: data.message, variant: 'destructive' })
+  if (!data.success) return toast({ description: data.message, variant: 'destructive' });
   localStorage.setItem('session', loginPassword.value)
   session.value = loginPassword.value
-  authStatus.value = false
+  authStatus.value = false;
+  // 站点列表
   getSiteList()
 }
 
-// 站点列表相关
+// 站点列表
 const siteList = ref<Array<string>>([])
 const siteValue = ref<string>('')
-const timeList = [
-  { name: 'Today', value: 'today' },
-  { name: 'Yesterday', value: '1d' },
-  { name: 'Last 7 days', value: '7d' },
-  { name: 'Last 30 days', value: '30d' },
-  { name: 'Last 60 days', value: '60d' },
-  { name: 'Last 90 days', value: '90d' }
-]
+const timeList = [{ name: 'Today', value: 'today' }, { name: 'Yesterday', value: '1d' }, { name: 'Last 7 days', value: '7d' }, { name: 'Last 30 days', value: '30d' }, { name: 'Last 60 days', value: '60d' }, { name: 'Last 90 days', value: '90d' }]
 const timeValue = ref<string>('today')
-
 const getSiteList = async () => {
   vh.showLoading()
   try {
-    const res = await fetch('/api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ type: 'list', session: session.value })
-    })
+    const res = await fetch('/api', { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify({ type: 'list', session: session.value }) })
     const data = await res.json()
     if (data.code && data.code === 401) {
       localStorage.clear()
       authStatus.value = true
     }
-    if (!data.success) return toast({ description: data.message, variant: 'destructive' })
-    siteList.value = data.data
+    if (!data.success) return toast({ description: data.message, variant: 'destructive' });
+    siteList.value = data.data;
     siteValue.value = siteList.value[0]
     if (siteValue.value) getDatas()
   } catch (error) {
-    console.log(error)
+    console.log(error);
   } finally {
     vh.hideLoading()
   }
 }
 
-// 获取数据相关
+// 获取数据
 const resData = ref<any>({ visit: {} })
 const tempResData = ref<any>({ visit: {} })
 const getDatasStatus = ref<boolean>(false)
 const getDatas = async () => {
+  // 清空数据
   resData.value = { visit: {} }
   tempResData.value = { visit: {} }
-  const pmsARR = ['visit', 'path', 'referrer', 'os', 'soft', 'area', 'echarts']
+  // 获取数据
+  const pmsARR = ['visit', 'path', 'referrer', 'os', 'soft', 'area', 'echarts'];
   getDatasStatus.value = true
   vh.showLoading()
-  const promisesForEach: Array<Promise<any>> = []
-
+  const promisesForEach: Array<Promise<any>> = [];
   pmsARR.forEach((i: any) => {
     const p = new Promise((r) => {
       (async () => {
         try {
           const pms = { type: i, siteID: siteValue.value, time: timeValue.value, session: session.value }
-          const res = await fetch('/api', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(pms)
-          })
+          const res = await fetch('/api', { method: 'POST', headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(pms) })
           const data = await res.json()
           if (data.code && data.code === 401) {
             localStorage.clear()
-            authStatus.value = true
+            authStatus.value = true;
           }
-          if (!data.success) return toast({ description: data.message, variant: 'destructive' })
-          tempResData.value[i] = i == 'echarts' ?
-            renderEcharts(
-              data.data.map((i: any) => `${i.name}${['today', '1d'].includes(timeValue.value) ? '点' : '日'}`),
-              data.data.map((i: any) => `${i.value}`)
-            ) : data.data
+          if (!data.success) return toast({ description: data.message, variant: 'destructive' });
+          tempResData.value[i] = i == 'echarts' ? renderEcharts(data.data.map((i: any) => `${i.name}${['today', '1d'].includes(timeValue.value) ? '点' : '日'}`), data.data.map((i: any) => `${i.value}`)) : data.data
         } catch (error) {
-          console.log(error)
+          console.log(error);
         } finally {
-          r(true)
+          // Promise执行完毕触发
+          r(true);
         }
-      })()
-    })
-    promisesForEach.push(p)
+      })();
+    });
+    promisesForEach.push(p);
   })
-
-  await Promise.all(promisesForEach)
-  getDatasStatus.value = false
+  await Promise.all(promisesForEach);
+  getDatasStatus.value = false;
   vh.hideLoading()
+  // 渲染数据
   resData.value = { ...tempResData.value }
 }
 
-// 工具函数
+// 获取ICON
 const getIconUrl = (url: string) => {
   if (!url) return 'https://icons.duckduckgo.com/ip3/none.ico'
   const _url = new URL(url)
   return `https://icons.duckduckgo.com/ip3/${_url.hostname}.ico`
 }
 
+// 获取Area ICON
 const getIcon = (code: string) => `${location.origin}/icon/${code}.png`
 
-// Echarts 相关
-const echartsDOM = ref<HTMLCanvasElement>()
-const canvasMain = ref<any>()
+// 渲染图表
+const echartsDOM = ref<HTMLCanvasElement>();
+const canvasMain = ref<any>();
 const renderEcharts = async (dateList: Array<any>, valueList: Array<any>) => {
   const option = {
     grid: { left: "0", right: "0", bottom: "0", top: "10", containLabel: true },
@@ -310,17 +437,18 @@ const renderEcharts = async (dateList: Array<any>, valueList: Array<any>) => {
         }
       }
     ]
-  }
-  canvasMain.value.setOption(option)
-}
+  };
+  canvasMain.value.setOption(option);
+};
 
 onMounted(() => {
-  canvasMain.value = markRaw(echarts.init(echartsDOM.value, null, { renderer: "svg", useDirtyRect: true }))
-  window.addEventListener("resize", canvasMain.value.resize)
+  //   图表
+  canvasMain.value = markRaw(echarts.init(echartsDOM.value, null, { renderer: "svg", useDirtyRect: true }));
+  window.addEventListener("resize", canvasMain.value.resize);
+  // 站点列表
   getSiteList()
 })
 </script>
-
 <style>
 .fixed.inset-0.z-50,
 .fixed.grid.w-full.max-w-lg.shadow-lg.duration-200 {
